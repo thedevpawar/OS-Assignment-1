843fb388ad8b4c75cb6c6f74d03302bd6f45ebfb
thedevpawar <142872236+thedevpawar@users.noreply.github.com>
2025-11-29 18:31:56 +0530

    task

---
+++ Assignment_2/task3.py
@@ -0,0 +1,27 @@
+import multiprocessing
+import logging
+import time
+
+logging.basicConfig(
+    filename='process_log.txt',
+    filemode='w',
+    level=logging. INFO,
+    format='%(asctime)s -%(processName)s -%(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+def system_process(task_name):
+    logging. info(f"{task_name} started")
+    time.sleep(2)
+    logging. info(f"{task_name} ended")
+
+if __name__ == "__main__":
+    print("System Starting ... ")
+
+    p1= multiprocessing.Process(target=system_process, args=("Process-1",))
+    p2 = multiprocessing.Process(target=system_process, args=("Process-2", ))
+
+    p1.start()
+    p2.start()
+
+    print("Processes started concurrently. Check process_log.txt.")
